433feb97224cd793776ca118adb279c01d011666
tpmac1990 <terence.paulmac@gmail.com>
2024-03-27 09:32:39 +1100

    setup sections collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,15 @@
 import {z, defineCollection} from "astro:content"
 
+const sectionCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      section: z.string(),
+      order: z.number(),
+      headers: z.array(z.string())
+    })
+});
 
 
 export const collections = {
-
+    sections: sectionCollection,
 }
